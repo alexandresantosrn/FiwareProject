a864899c0f11fea36d8d24b95f8beb2bde9d7437
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-06-25 14:00:51 -0300

    Commit inicial do projeto.

---
+++ Subscribe/src/main/java/jorge/ufrn/Subscribe/Controller/SubscribeController.java
@@ -0,0 +1,23 @@
+package jorge.ufrn.Subscribe.Controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+@RestController
+@RequestMapping("/subscribe")
+public class SubscribeController {
+
+	
+	@PostMapping
+	public void exibirSubscricao(@RequestBody JsonNode body) {
+		
+		
+		System.out.println("\n\n\n"+body.toPrettyString());
+		
+	}
+
+}
