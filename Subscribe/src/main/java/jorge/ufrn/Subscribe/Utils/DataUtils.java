e1acfb2ecbf9e39e33ce7fdc34e4fd626a57cd9f
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-06-25 22:42:57 -0300

    Corrigindo mensagem.

--- Subscribe/src/main/java/jorge/ufrn/Subscribe/Utils/DataUtils.java
+++ Subscribe/src/main/java/jorge/ufrn/Subscribe/Utils/DataUtils.java
@@ -3,19 +3,21 @@ package jorge.ufrn.Subscribe.Utils;
 public class DataUtils {
 
 	public static String formatCloth(String jsonResponse) {
-		
-		String word = "";		
-		String traje = jsonResponse.substring(181);
-		
-		for (int i = 0; i < traje.length(); i++) {
-		    char caractere = traje.charAt(i);
-		    if (caractere != '"') {
-		    	word += caractere;	
-		    }
+
+		String traje = "";
+		String word = jsonResponse.substring(184);
+
+		for (int i = 0; i < word.length(); i++) {
+			char caractere = word.charAt(i);
+			if (caractere != '"') {
+				traje += caractere;
+			} else {
+				break;
+			}
 		}
-		
-		String msg = "O traje: " + word + " encontra-se disponível para aluguel na Loja:";
-		
-		return msg;		
+
+		String msg = "O traje: " + traje + " encontra-se disponível para aluguel na Loja:";
+
+		return msg;
 	}
 }
